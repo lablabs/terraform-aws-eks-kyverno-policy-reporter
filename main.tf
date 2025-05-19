ed15d40932f61e45dc063372c0c3bb5ff94851ed
Attila Sovak <attila.sovak@gmail.com>
2025-05-19 15:42:38 +0200

    feat(kyverno-policy-reporter): add universal syntax changes

--- main.tf
+++ main.tf
@@ -6,32 +6,27 @@
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml)
  * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml)
  */
-# FIXME config: update addon docs above
-locals {
-  # FIXME config: add addon configuration here
-  addon = {
-    name = "universal-addon"
 
-    helm_chart_name    = "raw"
-    helm_chart_version = "0.1.0"
-    helm_repo_url      = "https://lablabs.github.io"
+locals {
+  addon = {
+    name = "policy-reporter"
+
+    helm_chart_version = "2.24.2"
+    helm_repo_url      = "https://kyverno.github.io/policy-reporter/"
   }
 
-  # FIXME config: add addon IRSA configuration here or remove if not needed
   addon_irsa = {
     (local.addon.name) = {
-      # FIXME config: add default IRSA overrides here or leave empty if not needed, but make sure to keep at least one key
-    }
-  }
-
-  # FIXME config: add addon OIDC configuration here or remove if not needed
-  addon_oidc = {
-    (local.addon.name) = {
-      # FIXME config: add default OIDC overrides here or leave empty if not needed, but make sure to keep at least one key
     }
   }
 
   addon_values = yamlencode({
-    # FIXME config: add default values here
+    serviceAccount = {
+      create = var.service_account_create != null ? var.service_account_create : true
+      name   = var.service_account_name != null ? var.service_account_name : local.addon.name
+      annotations = module.addon-irsa[local.addon.name].irsa_role_enabled ? {
+        "eks.amazonaws.com/role-arn" = module.addon-irsa[local.addon.name].iam_role_attributes.arn
+      } : tomap({})
+    }
   })
 }
