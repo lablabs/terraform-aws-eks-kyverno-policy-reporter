15ee0793bac2aac5d76e87296980043c9bff2ba1
Attila Sovak <attila.sovak@gmail.com>
2025-05-20 10:31:31 +0200

    Migrate to universal addon (kyverno-policy-reporter) (#3)
    * feat(kyverno-policy-reported): add init files
    
    * feat(kyverno-policy-reporter): add universal syntax changes
    
    * feat(kyverno-policy-reporter): add migrations
    
    * feat(kyverno-policy-reporter): fix docs

---
+++ main.tf
@@ -0,0 +1,32 @@
+/**
+ * # AWS EKS Kyverno policy reporter Terraform module
+ *
+ * A Terraform module to deploy the [Kyverno policy reporter](https://github.com/kyverno/policy-reporter) on Amazon EKS cluster.
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-kyverno-policy-reporter/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-kyverno-policy-reporter/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-kyverno-policy-reporter/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-kyverno-policy-reporter/actions/workflows/pre-commit.yml)
+ */
+
+locals {
+  addon = {
+    name = "policy-reporter"
+
+    helm_chart_version = "2.24.2"
+    helm_repo_url      = "https://kyverno.github.io/policy-reporter/"
+  }
+
+  addon_irsa = {
+    (local.addon.name) = {
+    }
+  }
+
+  addon_values = yamlencode({
+    serviceAccount = {
+      create = var.service_account_create != null ? var.service_account_create : true
+      name   = var.service_account_name != null ? var.service_account_name : local.addon.name
+      annotations = module.addon-irsa[local.addon.name].irsa_role_enabled ? {
+        "eks.amazonaws.com/role-arn" = module.addon-irsa[local.addon.name].iam_role_attributes.arn
+      } : tomap({})
+    }
+  })
+}
